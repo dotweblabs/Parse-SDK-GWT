fb1ab334599cab854e191fd7970da229482fe5cf
@kerbymart <kerbymart@gmail.com>
2017-11-19 04:34:01 +0800

    Added additional GwtMarshaller code block

--- src/main/java/org/parseplatform/client/GwtMarshaller.java
+++ src/main/java/org/parseplatform/client/GwtMarshaller.java
@@ -22,16 +22,14 @@ import org.parseplatform.util.DateUtil;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.parseplatform.client.util.LogUtil.log;
 
 
 public class GwtMarshaller implements Marshaller {
     //create model object
-    ParseObject parseMODEL = new ParseObject();
+    ParseObject parseModel = new ParseObject();
 
     @Override
     public ParseObject marshall(java.lang.Object instance) {
@@ -39,29 +37,15 @@ public class GwtMarshaller implements Marshaller {
         if (instance == null) {
             throw new RuntimeException("Object cannot be null");
         }
-        Class<?> declaringClass = instance.getClass();
-
-        //get objectID from model object
-        String objID = null;
-        Field objectFIELD = null;
-
-        //check if model object has objectId field
-        if (objID != null) {
-            // parseMODEL.putString("objectId", String.valueOf(objID));
-        }
-        //get type information of object in parameter
         //get fields in parameter object class
         Field[] fields = GwtReflect.getPublicFields(instance.getClass());
-
         //iterate for every annotated field since annotations will be done later change this to simple iterator
         for (int c = 0; c < fields.length; c++) {
             try {
                 //mutable class match to gettype class value
-                Class<?> fieldTYPE = fields[c].getType();
-
+                Class<?> fieldType = fields[c].getType();
                 //string type to string type
                 String fieldName = fields[c].getName();
-
                 Annotation[] testannotation = fields[c].getAnnotations();
                 for (int n = 0; n < testannotation.length; n++) {
                     //ignore all non @column
@@ -71,7 +55,7 @@ public class GwtMarshaller implements Marshaller {
                     if (annotationname.substring(annotationname.lastIndexOf('.') + 1) == "Column") {
                         java.lang.Object value = fields[c].get(instance);
                         //field name, field type, value conform to model object
-                        marshallValue(fieldName, fieldTYPE, value, parseMODEL);
+                        marshallValue(fieldName, fieldType, value, parseModel);
                     }
                 }
                 //object value to field object value
@@ -79,7 +63,7 @@ public class GwtMarshaller implements Marshaller {
                 Browser.getWindow().getConsole().warn("Error marshalling field " + fields[c].getName() + ":" + fields[c].getType().getName());
             }
         }
-        return parseMODEL;
+        return parseModel;
     }
 
     private static void marshallValue(String fieldName, Class<?> fieldType, java.lang.Object value, JSONObject parseObject) {
@@ -122,52 +106,54 @@ public class GwtMarshaller implements Marshaller {
                     parseObject.put(fieldName, new JSONNumber(Long.parseLong(value.toString())));
                 }
             } else if (fieldType.getName() == Date.class.getName()) {
-                log("Date type found");
                 JSONObject jsonDate = new JSONObject();
-                Date date = (Date) value;
-                jsonDate.put("__type", new JSONString("Date"));
-                jsonDate.put("iso", new JSONString(DateUtil.getStringFormat(date)));
-                parseObject.put(fieldName, jsonDate);
+                if(value instanceof Date) {
+                    Date date = (Date) value;
+                    jsonDate.put("__type", new JSONString("Date"));
+                    jsonDate.put("iso", new JSONString(DateUtil.getStringFormat(date)));
+                    parseObject.put(fieldName, jsonDate);
+                }
             } else if (fieldType.getName() == Map.class.getName()) { // Object
-                log("Map type found");
                 throw new RuntimeException("Map is not supported use com.parse.gwt.client.types.Object instead");
             } else if (fieldType.getName() == List.class.getName()) {
-                log("List type found");
                 throw new RuntimeException("List is not supported use com.parse.gwt.client.types.Array instead");
+            } else if (fieldType.getName() == LinkedList.class.getName()) {
+
             } else if (fieldType.getName() == File.class.getName()) {
-                log("File type found");
                 JSONObject jsonFile = new JSONObject();
-                File file = (File) value;
-                jsonFile.put("__type", new JSONString("File"));
-                jsonFile.put("url", new JSONString(file.url));
-                jsonFile.put("name", new JSONString(file.name));
-                parseObject.put(fieldName, jsonFile);
+                if(value != null && value instanceof File) {
+                    File file = (File) value;
+                    jsonFile.put("__type", new JSONString("File"));
+                    jsonFile.put("url", new JSONString(file.url));
+                    jsonFile.put("name", new JSONString(file.name));
+                    parseObject.put(fieldName, jsonFile);
+                }
             } else if (fieldType.getName() == GeoPoint.class.getName()) {
-                log("GeoPoint type found");
-                GeoPoint geoPoint = (GeoPoint) value;
-                ParseGeoPoint parseGeoPoint = new ParseGeoPoint(geoPoint.longitude, geoPoint.latitude);
-                parseObject.put(fieldName, parseGeoPoint);
+                if(value != null && value instanceof GeoPoint) {
+                    GeoPoint geoPoint = (GeoPoint) value;
+                    ParseGeoPoint parseGeoPoint = new ParseGeoPoint(geoPoint.longitude, geoPoint.latitude);
+                    parseObject.put(fieldName, parseGeoPoint);
+                }
             } else if (fieldType.getName() == Pointer.class.getName()) {
-                log("Pointer type found");
-                Pointer pointer = (Pointer) value;
-                JSONObject jsonPointer = new JSONObject();
-                jsonPointer.put("__type", new JSONString("Pointer"));
-                jsonPointer.put("className", new JSONString(pointer.className));
-                jsonPointer.put("objectId", new JSONString(pointer.objectId));
-                parseObject.put(fieldName, jsonPointer);
+                if(value != null && value instanceof Pointer) {
+                    Pointer pointer = (Pointer) value;
+                    JSONObject jsonPointer = new JSONObject();
+                    jsonPointer.put("__type", new JSONString("Pointer"));
+                    jsonPointer.put("className", new JSONString(pointer.className));
+                    jsonPointer.put("objectId", new JSONString(pointer.objectId));
+                    parseObject.put(fieldName, jsonPointer);
+                }
             } else if (fieldType.getName() == Relation.class.getName()) {
-                log("Relation type found");
-                Relation relation = (Relation) value;
-                JSONObject jsonRelation = new JSONObject();
-                jsonRelation.put("__type", new JSONString("Relation"));
-                jsonRelation.put("className", new JSONString(relation.className));
-                parseObject.put(fieldName, jsonRelation);
-            } else if (fieldType.getName() == Array.class.getName()) {
-                log("Array type found");
-                Array arrayValue = (Array) value;
-                parseObject.put(fieldName, (JSONArray) value);
+                if(value != null && value instanceof Relation) {
+                    Relation relation = (Relation) value;
+                    JSONObject jsonRelation = new JSONObject();
+                    jsonRelation.put("__type", new JSONString("Relation"));
+                    jsonRelation.put("className", new JSONString(relation.className));
+                    parseObject.put(fieldName, jsonRelation);
+                }
             } else if (fieldType.getName() == byte.class.getName()) {
-                Browser.getWindow().getConsole().warn("byte type found " + value.toString());
+                log("byte type found " + value.toString());
+                // TODO: What is this code for?
                 parseObject.put(fieldName, new JSONNumber(Integer.parseInt(value.toString())));
             } else if (fieldType.getName() == short.class.getName()) {
                 parseObject.put(fieldName, new JSONNumber((short) value));
@@ -190,31 +176,36 @@ public class GwtMarshaller implements Marshaller {
             } else if (fieldType.getName() == Byte[].class.getName()) {
                 parseObject.put(fieldName, new JSONNumber(Byte.parseByte(value.toString())));
             } else if (fieldType.getName() == ParseACL.class.getName()) {
-                //parseObject.put(fieldName, new JSONObject(value.));
-            } else if (fieldType.getName() == ParseCloud.class.getName()) {
-                //parseObject.put(fieldName, new JSONObject(value.));
-            } else if (fieldType.getName() == ParseConfig.class.getName()) {
-                //parseObject.put(fieldName, new JSONObject(value.));
-            } else if (fieldType.getName() == ParseConstants.class.getName()) {
-                //parseObject.put(fieldName, new JSONObject(value.));
+                if(value instanceof ParseACL) {
+                    parseObject.put(fieldName, (ParseACL) value);
+                }
             } else if (fieldType.getName() == ParseDate.class.getName()) {
-                //parseObject.put(fieldName, new JSONObject(value.));
-            } else if (fieldType.getName() == ParseError.class.getName()) {
-                //parseObject.put(fieldName, new JSONObject(value.));
+                if(value instanceof ParseDate) {
+                    parseObject.put(fieldName, (ParseDate) value);
+                }
+            } else if (fieldType.getName() == ParseFile.class.getName()) {
+                if(value instanceof ParseFile) {
+                    parseObject.put(fieldName, (ParseFile) value);
+                }
             } else if (fieldType.getName() == ParseGeoPoint.class.getName()) {
-                //parseObject.put(fieldName, new JSONObject(value.));
-            } else if (fieldType.getName() == ParseQuery.class.getName()) {
-                //parseObject.put(fieldName, new JSONObject(value.));
+                if(value instanceof ParseGeoPoint) {
+                    parseObject.put(fieldName, (ParseGeoPoint) value);
+                }
+            } else if (fieldType.getName() == ParsePointer.class.getName()) {
+                if(value instanceof ParsePointer) {
+                    parseObject.put(fieldName, (ParsePointer) value);
+                }
             } else if (fieldType.getName() == ParseRelation.class.getName()) {
-                //parseObject.put(fieldName, new JSONObject(value.));
-            } else if (fieldType.getName() == ParseResponse.class.getName()) {
-                //parseObject.put(fieldName, new JSONObject(value.));
+                if(value instanceof ParseRelation) {
+                    parseObject.put(fieldName, (ParseRelation) value);
+                }
             } else if (fieldType.getName() == ParseRole.class.getName()) {
-                //parseObject.put(fieldName, new JSONObject(value.));
-            } else if (fieldType.getName() == ParseUser.class.getName()) {
-                //parseObject.put(fieldName, new JSONObject(value.));
+                if(value instanceof ParseRole) {
+                    parseObject.put(fieldName, (ParseRole) value);
+                }
             } else {
-                //throw new RuntimeException("Unsupported type for field");
+                log("Unsupported type for field");
+                throw new RuntimeException("Unsupported type for field");
             }
         } else {
             parseObject.put(fieldName, JSONNull.getInstance());
