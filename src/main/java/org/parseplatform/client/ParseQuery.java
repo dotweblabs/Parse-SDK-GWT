3662533ac406747a100aa055c8d1e024c10f6a5d
Kerby Martino <kerby@dotweblabs.com>
2018-07-30 13:16:09 +0800

    Update ParseQuery from jsinterop-master branch

--- src/main/java/org/parseplatform/client/ParseQuery.java
+++ src/main/java/org/parseplatform/client/ParseQuery.java
@@ -1,5 +1,24 @@
 package org.parseplatform.client;
 
+/*
+File:  ParseQuery.java
+Version: 0-SNAPSHOT
+Contact: hello@dotweblabs.com
+----
+Copyright (c) 2018, Dotweblabs Web Technologies
+All rights reserved.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of Dotweblabs Web Technologies nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 import com.dotweblabs.shape.client.Shape;
 import com.google.common.base.Joiner;
 import com.google.gwt.http.client.URL;
@@ -8,10 +27,16 @@ import com.google.gwt.json.client.JSONNumber;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONString;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import elemental.client.Browser;
 
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * @author Kerby Martino
+ * @since 0-SNAPSHOT
+ * @version 0-SNAPSHOT
+ */
 public class ParseQuery extends JSONObject {
 
     private Where where;
@@ -20,6 +45,7 @@ public class ParseQuery extends JSONObject {
     private int skip;
 
     private List<String> includes;
+    private List<String> keys;
 
     public ParseQuery() {
     }
@@ -34,6 +60,20 @@ public class ParseQuery extends JSONObject {
 
     private void setClassName(String className) {
         put("className", new JSONString(className));
+    }
+
+    public ParseQuery key(String fieldName) {
+        if(keys == null) {
+            keys = new LinkedList<String>();
+        }
+        keys.add(fieldName);
+        return this;
+    }
+
+
+    public ParseQuery order(String key) {
+        put("order", new JSONString(key));
+        return this;
     }
 
     public ParseQuery order(Order order) {
@@ -51,6 +91,11 @@ public class ParseQuery extends JSONObject {
     public ParseQuery limit(int limit){
         this.limit = limit;
         put("limit", new JSONNumber(limit));
+        return this;
+    }
+
+    public ParseQuery count(int count) {
+        put("count", new JSONNumber(count));
         return this;
     }
 
@@ -85,18 +130,26 @@ public class ParseQuery extends JSONObject {
 
     public void find(final ParseAsyncCallback<ParseResponse> callback) {
         String stringIncludes = null;
+        String stringKeys = null;
         if(includes != null) {
             stringIncludes = Joiner.on(",").join(includes);
         }
+        if(keys != null && !keys.isEmpty()) {
+            stringKeys = Joiner.on(",").join(keys);
+        }
         String className = getClassName();
-        String order = "";
+        String order = null;
         String where = (get("where") != null && get("where").isObject() != null
                 && !get("where").isObject().toString().isEmpty()
                 && !get("where").isObject().toString().equals("{}")) ? "where=" + URL.encode(get("where").isObject().toString()) : null;
         String limit = null;
         String skip = null;
+        String count = null;
         if(get("limit") != null) {
             limit = get("limit").isNumber() != null ? "limit=" + ((int)get("limit").isNumber().doubleValue()) : null;
+        }
+        if(get("count") != null) {
+            count = get("count").isNumber() != null ? "count=" + ((int)get("count").isNumber().doubleValue()) : null;
         }
         if(get("skip") != null) {
             skip = get("skip").isNumber() != null ? "skip=" + ((int)get("skip").isNumber().doubleValue()) : null;
@@ -109,35 +162,70 @@ public class ParseQuery extends JSONObject {
             arrayString = arrayString.replaceAll("\"", "");
             order = order + URL.encode(arrayString);
         }
+        if(get("order") != null && get("order").isString() != null){
+            order = "order=" + get("order").isString().stringValue();
+        }
         String queryParams = "";
         // where + order + limit + skip
         if(where != null) {
             if(order != null) {
                 if(limit != null) {
                     if(skip != null) {
-                        queryParams = "?" + where + "&" + order + "&" + limit + "&" + skip;
+                        if(count != null) {
+                            queryParams = "?" + where + "&" + order + "&" + limit + "&" + skip + "&" + count;
+                        } else {
+                            queryParams = "?" + where + "&" + order + "&" + limit + "&" + skip;
+                        }
                     } else {
-                        queryParams = "?" + where + "&" + order + "&" + limit;
+                        if(count != null) {
+                            queryParams = "?" + where + "&" + order + "&" + limit  + "&" + count;
+                        } else {
+                            queryParams = "?" + where + "&" + order + "&" + limit;
+                        }
                     }
                 } else {
                     if(skip != null) {
-                        queryParams = "?" + where + "&" + order + "&" + skip;
+                        if(count != null) {
+                            queryParams = "?" + where + "&" + order + "&" + skip + "&" + count;
+                        } else {
+                            queryParams = "?" + where + "&" + order + "&" + skip;
+                        }
                     } else {
-                        queryParams = "?" + where + "&" + order;
+                        if(count != null) {
+                            queryParams = "?" + where + "&" + order + "&" + count;
+                        } else {
+                            queryParams = "?" + where + "&" + order;
+                        }
                     }
                 }
             } else {
                 if(limit != null) {
                     if(skip != null) {
-                        queryParams = "?" + where + "&" + limit + "&" + skip;
+                        if(count != null) {
+                            queryParams = "?" + where + "&" + limit + "&" + skip  + "&" + count;
+                        } else {
+                            queryParams = "?" + where + "&" + limit + "&" + skip;
+                        }
                     } else {
-                        queryParams = "?" + where + "&" + limit;
+                        if(count != null) {
+                            queryParams = "?" + where + "&" + limit + "&" + count;
+                        } else {
+                            queryParams = "?" + where + "&" + limit;
+                        }
                     }
                 } else {
                     if(skip != null) {
-                        queryParams = "?" + where + "&" + skip;
+                        if(count != null) {
+                            queryParams = "?" + where + "&" + skip + "&" + count;
+                        } else {
+                            queryParams = "?" + where + "&" + skip;
+                        }
                     } else {
-                        queryParams = "?" + where;
+                        if(count != null) {
+                            queryParams = "?" + where + "&" + count;
+                        } else {
+                            queryParams = "?" + where;
+                        }
                     }
                 }
             }
@@ -171,6 +259,13 @@ public class ParseQuery extends JSONObject {
                     }
                 }
             }
+            if(count != null) {
+                if(queryParams.contains("?")) {
+                    queryParams = queryParams + "&count=1";
+                } else {
+                    queryParams = queryParams + "?count=1";
+                }
+            }
         }
         //logger.info(Parse.SERVER_URL + Parse.CLASSES_URI + className + queryParams);
         String queryUrl = Parse.SERVER_URL + Parse.CLASSES_URI + className + queryParams;
@@ -179,6 +274,13 @@ public class ParseQuery extends JSONObject {
                 queryUrl = queryUrl + "?include=" + stringIncludes;
             } else {
                 queryUrl = queryUrl + "&include=" + stringIncludes;
+            }
+        }
+        if(stringKeys != null && !stringKeys.isEmpty()) {
+            if(queryUrl.equals(Parse.SERVER_URL + Parse.CLASSES_URI + className)) {
+                queryUrl = queryUrl + "?keys=" + stringKeys;
+            } else {
+                queryUrl = queryUrl + "&keys=" + stringKeys;
             }
         }
         Shape.get(queryUrl)
@@ -194,8 +296,12 @@ public class ParseQuery extends JSONObject {
                     @Override
                     public void onSuccess(String s) {
                         if(s != null && !s.isEmpty()) {
-                            ParseResponse resp = new ParseResponse(s);
-                            callback.onSuccess(resp);
+                            try {
+                                ParseResponse resp = new ParseResponse(s);
+                                callback.onSuccess(resp);
+                            } catch (Exception e) {
+                                callback.onFailure(new ParseError(e));
+                            }
                         } else {
                             callback.onFailure(new ParseError(204, "No response"));
                         }
@@ -207,4 +313,5 @@ public class ParseQuery extends JSONObject {
         Subscription subscription = new Subscription(getClassName(), where);
         return subscription;
     }
+
 }
