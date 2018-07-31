a0e13429b0df3da2527e9a3135c77009f4c625de
Kerby Martino <kerby@dotweblabs.com>
2018-07-31 20:52:29 +0800

    Added update Config from JSONObject params.

--- src/main/java/org/parseplatform/client/ParseConfig.java
+++ src/main/java/org/parseplatform/client/ParseConfig.java
@@ -59,4 +59,28 @@ public class ParseConfig {
                     }
                 });
     }
+    public static void update(JSONObject params, final ParseAsyncCallback<org.parseplatform.client.Config> callback) {
+        JSONObject body = new JSONObject();
+        body.put("params", params);
+        Shape.put(Parse.SERVER_URL + "/config")
+                .header("X-Parse-Application-Id", Parse.X_Parse_Application_Id)
+                .header("X-Parse-REST-API-Key", Parse.X_Parse_REST_API_Key)
+                .header("X-Parse-Master-Key", Parse.X_Parse_Master_Key)
+                .body(body.toString())
+                .asJson(new AsyncCallback<String>() {
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        callback.onFailure(new ParseError(throwable));
+                    }
+                    @Override
+                    public void onSuccess(String s) {
+                        try {
+                            org.parseplatform.client.Config config = org.parseplatform.client.Config.fromJSON(s);
+                            callback.onSuccess(config);
+                        } catch (Exception e){
+                            callback.onFailure(new ParseError(e));
+                        }
+                    }
+                });
+    }
 }
